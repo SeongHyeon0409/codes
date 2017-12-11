462554ede5c62b240576ce5f69d30bedbea80193
SeongHyeon0409 <31495131+SeongHyeon0409@users.noreply.github.com>
2017-12-11 10:22:26 +0900

    Add files via upload

---
+++ 창연공1/project3_student.py
@@ -0,0 +1,96 @@
+######################################################################
+### Date: 2017/10/5
+### file name: project3_student.py
+### Purpose: this code has been generated for the three-wheeled moving
+###         object to perform the project3 with ultra sensor
+###         swing turn, and point turn
+### this code is used for the student only
+######################################################################
+
+# =======================================================================
+# import GPIO library and time module
+# =======================================================================
+import RPi.GPIO as GPIO
+from time import sleep
+
+# =======================================================================
+#  set GPIO warnings as false
+# =======================================================================
+GPIO.setwarnings(False)
+
+# =======================================================================
+# import getDistance() method in the ultraModule
+# =======================================================================
+from ultraModule import getDistance
+
+# =======================================================================
+# import TurnModule() method
+# =======================================================================
+from TurnModule import *
+
+from trackingModule import *
+
+GPIO.setmode(GPIO.BOARD)
+
+# =======================================================================
+# rightPointTurn() and leftPointTurn() in TurnModule module
+# =======================================================================
+# student assignment (1)
+# student assignment (2)
+
+
+
+# =======================================================================
+# import go_forward_any(), go_backward_any(), stop(), LeftPwm(),
+# RightPwm(), pwm_setup(), and pwm_low() methods in the module of go_any
+# =======================================================================
+from go_any import *
+
+# implement rightmotor(x)  # student assignment (3)
+# implement go_forward_any(speed): # student assignment (4)
+# implement go_backward_any(speed): # student assignment (5)
+# implement go_forward(speed, running_time)  # student assignment (6)
+# implement go_backward(speed, running_time)  # student assignment (7)
+
+# =======================================================================
+# setup and init =ilaize the left motor and right motor
+# =======================================================================
+pwm_setup()
+
+# =======================================================================
+#  define your variables and find out each value of variables
+#  to perform the project3 with ultra sensor
+#  and swing turn
+# =======================================================================
+
+
+# when obstacle=1, the power and
+# running time of the first turn
+
+GPIOList = [GPIO.input(leftmostled),
+              GPIO.input(leftlessled),
+              GPIO.input(centerled),
+              GPIO.input(rightlessled),
+              GPIO.input(rightmostled)]
+
+try:
+
+        # when the distance is above the dis, moving object forwards
+
+        CurveTurn(40,40,0.1)
+
+
+
+
+
+
+
+                # when the distance is below the dis, moving object stops
+
+
+
+# when the Ctrl+C key has been pressed,
+# the moving object will be stopped
+
+except KeyboardInterrupt:
+    pwm_low()
---
+++ 창연공1/project4_linetracing.py
@@ -0,0 +1,163 @@
+#file name: project5_linetracing.py
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+from TurnModule import *
+
+from trackingModule import *
+
+from go_any import *
+
+pwm_setup()
+
+
+
+def getSensor():
+    sensor = [GPIO.input(leftmostled), GPIO.input(leftlessled), GPIO.input(centerled), GPIO.input(rightlessled),
+                    GPIO.input(rightmostled)]
+    return sensor
+
+
+try:
+    while True:
+        GPIOList = getSensor()
+        print(GPIOList)
+        CurveTurn(30,30,0.1)
+
+        #직진의 경우
+        if GPIOList == [1,1,0,1,1] or GPIOList == [1,0,0,0,1] or GPIOList == [0,1,0,0,1]:
+            CurveTurn_any(30,30)
+            print('straight')
+        elif GPIOList == [0,0,0,0,1]:
+            CurveTurn_any(25,28)
+        elif GPIOList == [1,0,0,1,1] or GPIOList == [1,0,1,1,1]:
+                #or GPIOList == [0,0,0,0,1]:
+            CurveTurn_any(20,40)
+            print('straignt and left')
+
+        #elif GPIOList == [0,1,1,1,1]:
+            #CurveTurn_any(20,28)
+
+        # elif GPIOList == [0,0,1,1,1]:
+        #     CurveTurn_any(20, 25)
+
+        elif GPIOList == [1,1,0,0,1]  or GPIOList == [1,1,1,0,1]:
+            CurveTurn_any(35,25)
+            print('straight and right')
+
+        elif GPIOList == [1,1,1,1,0]:
+             rightPointTurn(40,0.1) # 도는방향바낌
+        #elif GPIOList == [0,1,1,1,1]:
+            #leftPointTurn(30,0.1)
+
+        # 좌회전
+        elif GPIOList == [0,0,0,0,1] or GPIOList == [0,0,0,1,1] or GPIOList == [0,0,1,1,1] or GPIOList == [0,1,1,1,1]:
+            stop()
+            sleep(0.5)
+            print('좌회전')
+            # while GPIO.input(leftmostled) == 0:
+            #     CurveTurn_any(20,20)
+            CurveTurn(25,25,0.4)
+            while GPIO.input(leftmostled) == 0:
+                CurveTurn(25,25,0.1)
+                if GPIO.input(rightmostled) == 0:
+                    continue
+            stop()
+            sleep(0.1)
+            # 직진을 했을 시 a가 0일시 계속 직진
+            if GPIO.input(centerled) == 0:
+                print('left code out')
+                continue
+            else:
+                while GPIO.input(centerled) != 0:  # 직진라인에 딱 안착할때까지 우측으로 포인트 턴
+                    leftPointTurn(35, 0.0001)
+                    print('left1')  # leftturn
+                stop()
+                sleep(0.5)
+                list1 = getSensor()
+                print(list1)
+                # while GPIO.input(leftmostled) != 0 and GPIO.input(rightmostled) != 0:
+                #     leftPointTurn(20, 0.1)
+                #     print('라인 잘 탈때까지 좌회전')
+                # continue
+
+        #오른쪽 교차로시 앞으로 전진 후 턴 or T자 교차로 시 우회전
+        #elif GPIOList == [1,1,1,1,0] or GPIOList == [1,1,1,0,0] or GPIOList == [1,1,0,0,0] \
+                #or GPIOList == [1,0,0,0,0] or GPIOList == [0,0,0,0,0] or GPIOList == [0,1,0,0,0]:
+        elif GPIO.input(rightmostled) == 0:
+            stop()
+            sleep(0.5)
+            print('오른쪽 교차로시 앞으로 전진 후 턴 or T자 교차로 시 우회전')
+            CurveTurn(25,25,0.4)
+            stop()
+            sleep(0.1)
+            while GPIO.input(rightmostled) == 0:
+                CurveTurn_any(25,25)
+            stop()
+            sleep(0.1)
+            # if GPIO.input(centerled) == 0:
+
+            #     print('right code out')
+            #     continue
+            # else:
+
+            print('약간 전진후 스톱 그리고 라이트턴')
+            rightPointTurn(50,0.4)
+            print('0.5')
+            while GPIO.input(centerled) != 0: # 직진라인에 딱 안착할때까지 우측으로 포인트 턴
+                rightPointTurn(50,0.0001) #라이트턴
+                print('right 1')
+            while GPIO.input(centerled) != 0:
+                leftPointTurn(25, 0.0001)
+                print( 're turn')
+
+
+
+
+        #유턴, 오른쪽 90도 교차로 or 오른쪽 길이 없으면 좌회전
+        elif GPIOList == [1,1,1,1,1]:
+            stop()
+            sleep(1)
+            print('유턴, 오른쪽 90도 교차로 or 오른쪽 길이 없으면 좌회전')
+            CurveTurn(30,30,0.5)
+            while GPIO.input(centerled) != 0:
+                PointTurn(50, 0.0001)#라이트턴
+                print('uturn')
+            while GPIO.input(centerled) != 0:
+                leftPointTurn(25, 0.0001)
+            stop()
+            sleep(1)
+            continue
+        sleep(0.07)
+
+
+
+        #T자 교차로시
+        # elif GPIOList [0,0,0,0,0]:
+        #     stop()
+        #     sleep(1)
+        #     while GPIO.input(rightmostled) == 0:
+        #         go_forward_any(30)
+        #     stop()
+        #     sleep(1)
+        #     #직진을 했을 시 a가 0일시 계속 직진
+        #     if GPIO.input(centerled) == 0:
+        #         continue
+        #     else:
+        #         while GPIO.input(leftmostled) != 1 and GPIO.input(rightmostled) != 1 and GPIO.input(centerled) != 0: # 직진라인에 딱 안착할때까지 우측으로 포인트 턴
+        #             rightPointTurn(30,0.000000001) #라이트턴
+        #         stop()
+        #         sleep(1)
+        #         continue
+
+
+except KeyboardInterrupt:
+    pwm_low()
+
+
+
+
+
+
+
